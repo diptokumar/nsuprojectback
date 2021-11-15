2329dba932e50ca5a76df1c4592d4eacb655e2fa
diptokumar <diptakumar2@gmail.com>
2021-11-16 01:36:52 +0600

    jdnsfj

---
+++ models/userdataModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userdata = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email']
+  },
+  photo: String,
+  
+});
+module.exports = userdata
