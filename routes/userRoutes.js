17b52d4225c86a2fac071ec78fea727d8d32f0e9
diptokumar <diptakumar2@gmail.com>
2022-01-04 15:04:52 +0600

    routes added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const userController = require('./../controllers/userController');
 const authController = require('./../controllers/authController');
+const dataController = require('./../controllers/dataController');
+
 
 const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
+
+router.post('/datamodel', dataController.createdata);
+router.post('/getdata', dataController.getalldata);
+
+
 
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
